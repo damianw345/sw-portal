565d7b099a19f83a62371379849bc7cf5bb21e26
damianw345 <damianw345@gmail.com>
2019-04-15 10:03:21 +0200

    Refactor paginator component
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -12,14 +12,38 @@ export class PaginatorComponent implements OnInit {
   @Input()
   totalPages: number;
   @Input()
-  next: string;
-  @Input()
-  prev: string;
+  resourceType: string;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  private getRelativePagesNumbers(): Array<number> {
+    let pageNumbers = [];
+
+    switch (this.currentPage) {
+      // boundary cases
+      case 1:
+        pageNumbers = [0, 1, 2, 3, 4];
+        break;
+      case 2:
+        pageNumbers = [0, 1, 2, 3];
+        break;
+      case this.totalPages - 1:
+        pageNumbers = [-3, -2, -1, 0];
+        break;
+      case this.totalPages:
+        pageNumbers = [-4, -3, -2, -1, 0];
+        break;
+
+      // middle case
+      default:
+        pageNumbers = [-2, -1, 0, 1, 2];
+    }
+
+    return pageNumbers;
   }
 
 }

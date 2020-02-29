a78bf4fcda8223d26b185244407de5c16bea494d
damianw345 <damianw345@gmail.com>
2020-02-29 23:55:21 +0100

    Setup proxy.conf.json
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/app/core/http/swapi.service.ts
+++ src/app/core/http/swapi.service.ts
@@ -8,16 +8,16 @@ import { PageableResults } from '../model/pageable-results';
 })
 export class SwapiService {
 
-  private baseUrl = 'http://localhost:28080/api/swapi';
+  private endpoint = 'api/swapi';
 
   constructor(private http: HttpClient) { }
 
   getResources(type: string, pageId): Observable<PageableResults> {
-    return this.http.get<PageableResults>(`${this.baseUrl}/${type}?page=${pageId}`);
+    return this.http.get<PageableResults>(`${this.endpoint}/${type}?page=${pageId}`);
   }
 
   getResource(type: string, id: number): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/${type}/${id}`);
+    return this.http.get<any>(`${this.endpoint}/${type}/${id}`);
   }
 
 }

db2c3cea327f53902f3f0b7edd56207d2b68eb8e
damianw345 <damianw345@gmail.com>
2020-07-05 19:01:07 +0200

    Prevent logout after page refresh
    Signed-off-by: damianw345 <damianw345@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from './core/service/user.service';
+import { AuthService } from './core/service/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sw-portal';
-
-  constructor(public userService: UserService) {
+export class AppComponent implements OnInit {
+  constructor(public userService: UserService,
+              private authService: AuthService) {
   }
 
   logout(): void {
     this.userService.logout();
+  }
+
+  ngOnInit(): void {
+    const token = this.authService.getTokenFromStorage();
+    if (token) {
+      this.userService.loginWithToken(token);
+    }
   }
 }
